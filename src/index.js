ff0abbc9f65fbdf126a38cf869a77b9044530f22
Savio <iamsavio19@gmail.com>
2023-05-08 22:27:06 +0530

    fitst component created

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const rootelement = document.getElementById("root");
+function App() {
+  return <h1>FLAMES</h1>;
+}
+const root = ReactDOM.createRoot(rootelement);
+root.render(<App />);
