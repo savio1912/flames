90c41d75945f48fc3f9f2b7e0e9d68ae06bc1ced
Savio <iamsavio19@gmail.com>
2023-05-09 16:26:04 +0530

    Flames function added

---
+++ src/Flames.js
@@ -0,0 +1,57 @@
+const Flames = (props) => {
+  function Unique_Count(name1, name2) {
+    name1 = name1.toLowerCase().replace(/\s/g, "");
+    name2 = name2.toLowerCase().replace(/\s/g, "");
+    let n1 = (name1.match(/ /g) || []).length;
+    let n2 = (name2.match(/ /g) || []).length;
+    name1 = name1.replace(/\s/g, "").substring(0, name1.length - n1);
+    name2 = name2.replace(/\s/g, "").substring(0, name2.length - n2);
+    console.log(name1);
+    console.log(name2);
+    let name1_count = {};
+    let name2_count = {};
+    let Count = 0;
+    for (let ch of name1) {
+      name1_count[ch] = (name1_count[ch] || 0) + 1;
+    }
+    for (let ch of name2) {
+      if (name1_count[ch] !== undefined && name1_count[ch] !== 0) {
+        name1_count[ch]--;
+      } else {
+        name2_count[ch] = (name2_count[ch] || 0) + 1;
+      }
+    }
+    for (let key in name1_count) {
+      Count += name1_count[key];
+    }
+    for (let key in name2_count) {
+      Count += name2_count[key];
+    }
+    return Count;
+  }
+
+  function flames(Count) {
+    let f = "FLAMES";
+    let Size = f.length;
+    let i = 0;
+    while (f.length > 1) {
+      let rem = (i + Count) % Size;
+      if (rem === 0) {
+        f = f.substring(0, f.length - 1);
+        i = 0;
+      } else {
+        f = f.substring(0, rem - 1) + f.substring(rem);
+        i = rem - 1;
+      }
+      Size = f.length;
+    }
+    return f[0];
+  }
+
+  let { name1, name2 } = props;
+  const count = Unique_Count(name1, name2);
+  const ch = flames(count);
+
+  return <h1>Result{ch}</h1>;
+};
+export default Flames;
--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Flames from "./Flames";
 
 const Form = () => {
   const [name1, setName1] = useState("");
@@ -9,27 +10,35 @@ const Form = () => {
   function eventHandler2(event) {
     setName2(event.target.value);
   }
+  const formHandler = (event) => {
+    event.preventDefault();
+    console.log(name1 + " " + name2);
+  };
 
   return (
-    <form>
-      <label htmlFor="name1">Name 1</label>
-      <input
-        type="text"
-        id="name1"
-        name="name1"
-        value={name1}
-        onChange={eventHandler1}
-      ></input>
-      <label htmlFor="name2">Name 2</label>
-      <input
-        type="text"
-        id="name2"
-        name="name2"
-        value={name2}
-        onChange={eventHandler2}
-      ></input>
-      <input type="submit" />
-    </form>
+    <>
+      {" "}
+      <form onSubmit={formHandler}>
+        <label htmlFor="name1">Name 1</label>
+        <input
+          type="text"
+          id="name1"
+          name="names1"
+          value={name1}
+          onChange={eventHandler1}
+        ></input>
+        <label htmlFor="name2">Name 2</label>
+        <input
+          type="text"
+          id="name2"
+          name="name2"
+          value={name2}
+          onChange={eventHandler2}
+        ></input>
+        <input type="submit" />
+      </form>
+      <Flames name1={name1.toLowerCase()} name2={name2.toLowerCase()} />
+    </>
   );
 };
 export default Form;
