dbfa6cbc64873f7468e7b83cc251a48978e3338a
Savio <iamsavio19@gmail.com>
2023-05-15 16:40:32 +0530

    auth done

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Form from "./Components/Form.js";
 import "./index.css";
 import Dashboard from "./Components/Dashboard.js";
+import Signup from "./Auth/Signup.js";
+import Login from "./Auth/Login.js";
 
 const App = () => {
   return (
     <BrowserRouter>
       <main>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Signup />} />
           <Route path="/flames" element={<Form />} />
-          {/* <Route
-            path="/result"
-            element={
-              <Flames name1={n1.toLowerCase()} name2={n2.toLowerCase()} />
-            }
-          /> */}
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/login" element={<Login />}></Route>
         </Routes>
       </main>
       <footer className="footer">All rights reserved</footer>
